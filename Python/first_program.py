c4a9128fa15f809fbd5a17b5c7f39e78d1b575c9
Arnab Sural <arnabsural1890@gmail.com>
2026-09-05 09:28:03 +0530

    Add student pass prediction data

--- Python/first_program.py
+++ Python/first_program.py
@@ -1,27 +1,14 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-# Training data
 data = {
     "hours": [1, 2, 3, 4, 5, 6, 7],
     "result": ["Fail", "Fail", "Fail", "Pass", "Pass", "Pass", "Pass"]
 }
 
-# Convert data into DataFrame
 df = pd.DataFrame(data)
 
-# Input and output
-X = df[["hours"]]
+print(df)
+
+x = df[["hours"]]
 y = df["result"]
-
-# Create the ML model
-model = DecisionTreeClassifier()
-
-# Train the model
-model.fit(X, y)
-
-# Make prediction
-prediction = model.predict([[5]])
-
-# Display prediction
-print("Prediction:", prediction[0])
